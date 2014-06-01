4ef8137be0a913294d78ed08dbaf64eee51abdf3
Guillaume BILLEY <guillaume.billey@gmail.com>
2014-06-01 16:10:23 +0200

    Empty maven project

---
+++ src/main/java/fr/guilbill/App.java
@@ -0,0 +1,13 @@
+package fr.guilbill;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
