b466a749c88b4156d9563d71165f562efec6c622
Guillaume BILLEY <guillaume.billey@gmail.com>
2014-06-01 16:53:39 +0200

    <feat>(First test, compile) Making the first test to compile, but fail

---
+++ src/main/java/fr/guilbill/Dollar.java
@@ -0,0 +1,20 @@
+package fr.guilbill;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: guillaume
+ * Date: 01/06/14
+ * Time: 16:49
+ * To change this template use File | Settings | File Templates.
+ */
+public class Dollar {
+    public int amount;
+
+    public Dollar(int i) {
+        //To change body of created methods use File | Settings | File Templates.
+    }
+
+    public void time(int i) {
+        //To change body of created methods use File | Settings | File Templates.
+    }
+}
